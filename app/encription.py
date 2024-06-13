e862bf60923fbb8e952a998049fd7a2aa2f3d234
Nicholas Muriithi <nickstech707@gmail.com>
2024-06-13 13:03:55 +0300

    Refactored code for better structure and readability

---
+++ app/encription.py
@@ -0,0 +1,13 @@
+# from cryptography.fernet import Fernet
+
+# def load_key():
+#     return open("secret.key", "rb").read()
+
+# key = load_key()
+# cipher_suite = Fernet(key)
+
+# def encrypt(text):
+#     return cipher_suite.encrypt(text.encode()).decode()
+
+# def decrypt(encrypted_text):
+#     return cipher_suite.decrypt(encrypted_text.encode()).decode()
